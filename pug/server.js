f02eea47f2ae756788559c97e446e6c9e4d1a3e0
Carola Cimini <caro.cimini@gmail.com>
2022-08-11 12:13:51 -0300

    correcciones y segunda parte desafio clase 10 con pug

--- pug/server.js
+++ pug/server.js
@@ -1,32 +1,20 @@
 const Contenedor = require("./contenedor")
 const contenedor = new Contenedor('./productos.txt')
 
-const handlebars = require('express-handlebars')
-
 const express =require('express')
 
 const app = express()
 
-
 app.use(express.json())
-app.use(express.urlencoded({ extended: false}))
 app.use(express.static('public'))
+app.use(express.urlencoded({ extended: false}))
 
-app.engine(
-    'hbs', 
-    handlebars.engine({
-        extname: '.hbs',  
-        defaultLayout: 'main.hbs' ,          
-        layoutsDir: __dirname + '/views',
-        partialsDir: __dirname + '/views/partials'
-    })
-)
+app.set('view engine', 'pug')
+app.set('views', './views')
 
-app.set('view engine', 'hbs')
-app.set('views', './views/layouts')
 
 app.get('/', async (req, res) => {
-    res.render('prodAdmin',{
+    res.render('prodAdmin.pug',{
         titulo: "Tabaqueria Blend&roll",
         catalogo: false
     })
